47770d161811b1a35c5b0adb7f1a751965e89da8
rubendepaz1 <rubendepaz1@gmail.com>
2022-03-06 21:15:34 +0100

    Update correccion-vars.tf

--- terraform/correccion-vars.tf
+++ terraform/correccion-vars.tf
@@ -22,8 +22,14 @@ variable "ssh_user" {
   default = "adminUsername"
 }
 
-variable "vm_size" {
+variable "vm_worker" {
   type = string
-  description = "Tamaño de la máquina virtual"
+  description = "Tamaño de la máquina virtual para el worker"
   default = "Standard_D1_v2" # 3.5 GB, 1 CPU 
+}
+
+variable "vm_worker" {
+  type = string
+  description = "Tamaño de la máquina virtual para el master"
+  default = "Standard_D2_v2" # 7 GB, 2 CPU 
 }
