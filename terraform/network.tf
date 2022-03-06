eeac402af6b44faee100008dce5434d3da81e01c
rubendepaz1 <rubendepaz1@gmail.com>
2022-03-06 20:35:12 +0100

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -23,7 +23,7 @@ resource "azurerm_subnet" "mySubnet" {
 
 }
 
-# Create NIC
+# Create NIC para el MASTER
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_interface
 
 resource "azurerm_network_interface" "myNic1" {
@@ -45,11 +45,49 @@ resource "azurerm_network_interface" "myNic1" {
 
 }
 
-# IP pública
+# IP pública para el MASTER
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/public_ip
 
 resource "azurerm_public_ip" "myPublicIp1" {
   name                = "vmip1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
+
+# Create NIC para el WORKER
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_interface
+
+resource "azurerm_network_interface" "myNic2" {
+  name                = "vmnic2"  
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+    ip_configuration {
+    name                           = "myipconfiguration2"
+    subnet_id                      = azurerm_subnet.mySubnet.id 
+    private_ip_address_allocation  = "Static"
+    private_ip_address             = "10.0.1.11"
+    public_ip_address_id           = azurerm_public_ip.myPublicIp2.id
+  }
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
+
+# IP pública para el WORKER
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/public_ip
+
+resource "azurerm_public_ip" "myPublicIp2" {
+  name                = "vmip2"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
