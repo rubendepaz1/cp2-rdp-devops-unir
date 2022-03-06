bd5ecd20a3e5d9c757c7c4f0c3ec2cc5dc4d24ed
rubendepaz1 <rubendepaz1@gmail.com>
2022-03-06 18:23:52 +0100

    Update vm.tf

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -1,8 +1,51 @@
-# Creamos una máquina virtual
+# Creamos la vm que será nuestro MASTER
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine
 
 resource "azurerm_linux_virtual_machine" "myVM1" {
     name                = "master"
+    resource_group_name = azurerm_resource_group.rg.name
+    location            = azurerm_resource_group.rg.location
+    size                = var.vm_size
+    admin_username      = "adminUsername"
+    network_interface_ids = [ azurerm_network_interface.myNic1.id ]
+    disable_password_authentication = true
+
+    admin_ssh_key {
+        username   = "adminUsername"
+        public_key = file("~/.ssh/id_rsa.pub")
+    }
+
+    os_disk {
+        caching              = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
+
+    plan {
+        name      = "centos-8-stream-free"
+        product   = "centos-8-stream-free"
+        publisher = "cognosys"
+    }
+
+    source_image_reference {
+        publisher = "cognosys"
+        offer     = "centos-8-stream-free"
+        sku       = "centos-8-stream-free"
+        version   = "1.2019.0810"
+    }
+
+    boot_diagnostics {
+        storage_account_uri = azurerm_storage_account.stAccount.primary_blob_endpoint
+    }
+
+    tags = {
+        environment = "CP2"
+    }
+    
+# Creamos la vm que será nuestro WORKER
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine
+
+resource "azurerm_linux_virtual_machine" "myVM2" {
+    name                = "worker"
     resource_group_name = azurerm_resource_group.rg.name
     location            = azurerm_resource_group.rg.location
     size                = var.vm_size
