0f9dd94d390f82c229fe6253337a653eb884549b
rubendepaz1 <rubendepaz1@gmail.com>
2022-03-06 20:35:47 +0100

    Update security.tf

--- terraform/security.tf
+++ terraform/security.tf
@@ -31,3 +31,9 @@ resource "azurerm_network_interface_security_group_association" "mySecGroupAssoc
     network_security_group_id = azurerm_network_security_group.mySecGroup.id
 
 }
+
+resource "azurerm_network_interface_security_group_association" "mySecGroupAssociation2" {
+    network_interface_id      = azurerm_network_interface.myNic2.id
+    network_security_group_id = azurerm_network_security_group.mySecGroup.id
+
+}
