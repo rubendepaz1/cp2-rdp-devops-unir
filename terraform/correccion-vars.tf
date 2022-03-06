93dca77d539eeb2ff50abc4d7e59d7d92f100308
rubendepaz1 <rubendepaz1@gmail.com>
2022-03-06 21:15:46 +0100

    Update correccion-vars.tf

--- terraform/correccion-vars.tf
+++ terraform/correccion-vars.tf
@@ -28,7 +28,7 @@ variable "vm_worker" {
   default = "Standard_D1_v2" # 3.5 GB, 1 CPU 
 }
 
-variable "vm_worker" {
+variable "vm_master" {
   type = string
   description = "Tamaño de la máquina virtual para el master"
   default = "Standard_D2_v2" # 7 GB, 2 CPU 
