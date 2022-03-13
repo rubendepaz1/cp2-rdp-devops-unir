53e3e317dd86c200e6fd34ab6cb3ea8f8a4ea741
Rubén de Paz <ruben.depaz@ezentis.com>
2022-03-13 18:29:17 +0100

    puertos all hosts

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -5,7 +5,7 @@
 # https://github.com/jadebustos/devopslabs/blob/master/labs-k8s/00-00-instalando-kubernetes.md#instalaci%C3%B3n-del-servidor-nfs
 # ansible-playbook -i hosts -l nfs servidor-NFS.yaml
 # https://github.com/jadebustos/devopslabs/blob/master/labs-k8s/00-00-instalando-kubernetes.md#tareas-comunes-a-realizar-en-el-nodo-master-y-los-workers
-# ansible-playbook -i hosts tareas-comunes-master-worker.yaml
+ansible-playbook -i hosts tareas-comunes-master-worker.yaml
 # https://github.com/jadebustos/devopslabs/blob/master/labs-k8s/00-00-instalando-kubernetes.md#configurando-kubernetes-en-el-nodo-master
 ansible-playbook -i hosts -l master tareas-master.yaml
 # https://github.com/jadebustos/devopslabs/blob/master/labs-k8s/00-00-instalando-kubernetes.md#instalando-la-sdn
