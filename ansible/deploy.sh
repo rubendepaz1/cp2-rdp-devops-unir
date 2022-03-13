b91680d0c49bdf2cce6e5cc34306708cff1f56ff
Rubén de Paz <ruben.depaz@ezentis.com>
2022-03-14 00:16:44 +0100

    wip

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # https://github.com/jadebustos/devopslabs/blob/master/labs-k8s/00-00-instalando-kubernetes.md#tareas-previas-de-configuraci%C3%B3n
-ansible-playbook -i hosts tareas-previas.yaml
+#ansible-playbook -i hosts tareas-previas.yaml
 # https://github.com/jadebustos/devopslabs/blob/master/labs-k8s/00-00-instalando-kubernetes.md#instalaci%C3%B3n-del-servidor-nfs
-ansible-playbook -i hosts -l nfs servidor-NFS.yaml
+#ansible-playbook -i hosts -l nfs servidor-NFS.yaml
 # https://github.com/jadebustos/devopslabs/blob/master/labs-k8s/00-00-instalando-kubernetes.md#tareas-comunes-a-realizar-en-el-nodo-master-y-los-workers
-ansible-playbook -i hosts tareas-comunes-master-worker.yaml
+#ansible-playbook -i hosts tareas-comunes-master-worker.yaml
 # https://github.com/jadebustos/devopslabs/blob/master/labs-k8s/00-00-instalando-kubernetes.md#configurando-kubernetes-en-el-nodo-master
 ansible-playbook -i hosts -l master tareas-master.yaml
 # https://github.com/jadebustos/devopslabs/blob/master/labs-k8s/00-00-instalando-kubernetes.md#instalando-la-sdn
