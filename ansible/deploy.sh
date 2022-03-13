764aedb8a13857662d848976018328956a560400
Rubén <rubendepaz1@gmail.com>
2022-03-13 17:21:21 +0100

    tareas master

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -5,9 +5,9 @@
 # https://github.com/jadebustos/devopslabs/blob/master/labs-k8s/00-00-instalando-kubernetes.md#instalaci%C3%B3n-del-servidor-nfs
 # ansible-playbook -i hosts -l nfs servidor-NFS.yaml
 # https://github.com/jadebustos/devopslabs/blob/master/labs-k8s/00-00-instalando-kubernetes.md#tareas-comunes-a-realizar-en-el-nodo-master-y-los-workers
-ansible-playbook -i hosts tareas-comunes-master-worker.yaml
+# ansible-playbook -i hosts tareas-comunes-master-worker.yaml
 # https://github.com/jadebustos/devopslabs/blob/master/labs-k8s/00-00-instalando-kubernetes.md#configurando-kubernetes-en-el-nodo-master
-# ansible-playbook -i hosts master tareas-master.yaml
+ansible-playbook -i hosts master tareas-master.yaml
 # https://github.com/jadebustos/devopslabs/blob/master/labs-k8s/00-00-instalando-kubernetes.md#instalando-la-sdn
 # ansible-playbook -i hosts master sdn.yaml
 # https://github.com/jadebustos/devopslabs/blob/master/labs-k8s/00-00-instalando-kubernetes.md#desplegando-un-ingress-controller
